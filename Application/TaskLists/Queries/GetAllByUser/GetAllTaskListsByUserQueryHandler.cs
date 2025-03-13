9c988027cb6793db0cdbe03aa60e5b2308dba24c
Kevin Torrecilla <kevin.torrecilla@softwareone.com>
2025-03-13 13:06:41 -0500

    feat(tasklist): agregar consultas y controlador para gestionar listas de tareas

---
+++ Application/TaskLists/Queries/GetAllByUser/GetAllTaskListsByUserQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Application.Data.Repositories;
+using Application.TaskLists.Dtos;
+using ErrorOr;
+using MediatR;
+
+namespace Application.TaskLists.Queries.GetAllByUser
+{
+    internal sealed class GetAllTaskListsByUserQueryHandler : IRequestHandler<GetAllTaskListsByUserQuery, ErrorOr<List<TaskListDTO>>>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public GetAllTaskListsByUserQueryHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+        }
+
+        public async Task<ErrorOr<List<TaskListDTO>>> Handle(GetAllTaskListsByUserQuery query, CancellationToken cancellationToken)
+        {
+            var userDto = await _userRepository.GetByIdAsync(query.UserId);
+
+            if (userDto == null)
+            {
+                return Error.NotFound("User.NotFound", "The user with the provided Id was not found.");
+            }
+
+            return userDto.TaskLists;
+        }
+    }
+}
