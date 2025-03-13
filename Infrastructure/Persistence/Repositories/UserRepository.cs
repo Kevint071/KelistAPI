9601824c37e17c46a5fecaad69971565402493f9
Kevin Torrecilla <kevin.torrecilla@softwareone.com>
2025-03-13 13:05:19 -0500

    feat(user): agregar método para asociar listas de tareas a un usuario en el repositorio

--- Infrastructure/Persistence/Repositories/UserRepository.cs
+++ Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Application.Users.Dtos;
+using Application.TaskLists.Dtos;
 
 namespace Infrastructure.Persistence.Repositories
 {
@@ -18,6 +19,20 @@ namespace Infrastructure.Persistence.Repositories
         public void Delete(UserDTO userDto) => _context.Users.Remove(userDto);
         public async Task<List<UserDTO>> GetAll() => await _context.Users.ToListAsync();
         public async Task<bool> ExistsAsync(Guid id) => await _context.Users.AnyAsync(c => c.Id == id);
-        public async Task<UserDTO?> GetByIdAsync(Guid id) => await _context.Users.SingleOrDefaultAsync(c => c.Id == id);
+        public async Task<UserDTO?> GetByIdAsync(Guid id) => await _context.Users.Include(x => x.TaskLists).SingleOrDefaultAsync(c => c.Id == id);
+
+        public void AddTaskListToUser(Guid userId, TaskListDTO taskListDto)
+        {
+            var userDto = _context.Users
+                .Include(u => u.TaskLists)
+                .SingleOrDefault(u => u.Id == userId);
+
+            if (userDto != null)
+            {
+                userDto.TaskLists.Add(taskListDto);
+                // Asegurarnos de que EF Core la trate como una entidad nueva
+                _context.Entry(taskListDto).State = EntityState.Added;
+            }
+        }
     }
 }
