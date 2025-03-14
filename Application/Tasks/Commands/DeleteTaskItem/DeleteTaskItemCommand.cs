ceb63b05e6b8f42d2342e2a5e9d0afc1b6b8dc30
Kevin Torrecilla <kevin.torrecilla@softwareone.com>
2025-03-14 15:07:50 -0500

    feat: agregar estructura de comando DeleteTaskItem

---
+++ Application/Tasks/Commands/DeleteTaskItem/DeleteTaskItemCommand.cs
@@ -0,0 +1,7 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace Application.Tasks.Commands.DeleteTaskItem
+{
+    public record DeleteTaskItemCommand(Guid UserId, Guid TaskListId, Guid TaskItemId) : IRequest<ErrorOr<Unit>>;
+}
---
+++ Application/Tasks/Commands/DeleteTaskItem/DeleteTaskItemCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using Application.Data.Interfaces;
+using Application.Data.Repositories;
+using ErrorOr;
+using MediatR;
+
+namespace Application.Tasks.Commands.DeleteTaskItem
+{
+    internal sealed class DeleteTaskItemCommandHandler : IRequestHandler<DeleteTaskItemCommand, ErrorOr<Unit>>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteTaskItemCommandHandler(IUserRepository userRepository, IUnitOfWork unitOfWork)
+        {
+            _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+            _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+        }
+
+        public async Task<ErrorOr<Unit>> Handle(DeleteTaskItemCommand command, CancellationToken cancellationToken)
+        {
+            var userDto = await _userRepository.GetByIdAsync(command.UserId);
+            if (userDto == null)
+            {
+                return Error.NotFound("User.NotFound", "The user with the provided Id was not found.");
+            }
+
+            var taskListDto = userDto.TaskLists.FirstOrDefault(tl => tl.Id == command.TaskListId);
+            if (taskListDto == null)
+            {
+                return Error.NotFound("TaskList.NotFound", "The task list with the provided Id was not found.");
+            }
+
+            var taskItemDto = taskListDto.TaskItems.FirstOrDefault(ti => ti.Id == command.TaskItemId);
+            if (taskItemDto == null)
+            {
+                return Error.NotFound("TaskItem.NotFound", "The task item with the provided Id was not found.");
+            }
+
+            _userRepository.DeleteTaskItem(command.UserId, command.TaskListId, command.TaskItemId);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
