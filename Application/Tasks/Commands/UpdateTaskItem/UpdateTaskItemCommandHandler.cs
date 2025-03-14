babbf892a39eb01a9eb7441d993f74981c8ba233
Kevin Torrecilla <kevin.torrecilla@softwareone.com>
2025-03-14 15:07:51 -0500

    feat: agregar estructura de comando UpdateTaskItem

---
+++ Application/Tasks/Commands/UpdateTaskItem/UpdateTaskItemCommandHandler.cs
@@ -0,0 +1,54 @@
+﻿using Application.Data.Interfaces;
+using Application.Data.Repositories;
+using Application.Tasks.Dtos;
+using ErrorOr;
+using MediatR;
+
+namespace Application.Tasks.Commands.UpdateTaskItem
+{
+    internal sealed class UpdateTaskItemCommandHandler : IRequestHandler<UpdateTaskItemCommand, ErrorOr<Unit>>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpdateTaskItemCommandHandler(IUserRepository userRepository, IUnitOfWork unitOfWork)
+        {
+            _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+            _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+        }
+
+        public async Task<ErrorOr<Unit>> Handle(UpdateTaskItemCommand command, CancellationToken cancellationToken)
+        {
+            // Obtener el usuario con sus TaskLists
+            var userDto = await _userRepository.GetByIdAsync(command.UserId);
+            if (userDto == null)
+            {
+                return Error.NotFound("User.NotFound", "The user with the provided Id was not found.");
+            }
+
+            // Verificar si la TaskList existe
+            var taskListDto = userDto.TaskLists.FirstOrDefault(tl => tl.Id == command.TaskListId);
+            if (taskListDto == null)
+            {
+                return Error.NotFound("TaskList.NotFound", "The task list with the provided Id was not found.");
+            }
+
+            var taskItemDto = taskListDto.TaskItems.FirstOrDefault(ti => ti.Id == command.TaskItemId);
+            if (taskItemDto == null)
+            {
+                return Error.NotFound("TaskItem.NotFound", "The task item with the provided Id was not found.");
+            }
+
+            var updatedTaskItemDto = new TaskItemDTO
+            {
+                Id = command.TaskItemId,
+                Description = command.Description,
+                IsCompleted = command.IsCompleted
+            };
+
+            _userRepository.UpdateTaskItem(command.UserId, command.TaskListId, updatedTaskItemDto);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
