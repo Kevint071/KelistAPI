0b8d3e9f2525adf281fb36b8719f4e99620a35d7
Kevin Torrecilla <kevin.torrecilla@softwareone.com>
2025-03-14 15:07:54 -0500

    feat: agregar estructura de consultas para tareas

---
+++ Application/Tasks/Queries/GetAllByTaskList/GetAllTaskItemsByTaskListQuery.cs
@@ -0,0 +1,8 @@
+﻿using Application.Tasks.Dtos;
+using ErrorOr;
+using MediatR;
+
+namespace Application.Tasks.Queries.GetAllByTaskListId
+{
+    public record GetAllTaskItemsByTaskListQuery(Guid UserId, Guid TaskListId) : IRequest<ErrorOr<List<TaskItemDTO>>>;
+}
---
+++ Application/Tasks/Queries/GetAllByTaskList/GetAllTaskItemsByTaskListQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Data.Repositories;
+using Application.Tasks.Dtos;
+using ErrorOr;
+using MediatR;
+
+namespace Application.Tasks.Queries.GetAllByTaskListId
+{
+    internal sealed class GetAllTaskItemsByTaskListQueryHandler : IRequestHandler<GetAllTaskItemsByTaskListQuery, ErrorOr<List<TaskItemDTO>>>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public GetAllTaskItemsByTaskListQueryHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+        }
+
+        public async Task<ErrorOr<List<TaskItemDTO>>> Handle(GetAllTaskItemsByTaskListQuery query, CancellationToken cancellationToken)
+        {
+            var userDto = await _userRepository.GetByIdAsync(query.UserId);
+
+            if (userDto == null)
+            {
+                return Error.NotFound("User.NotFound", "The user with the provided Id was not found.");
+            }
+
+            var taskListDto = userDto.TaskLists.FirstOrDefault(tl => tl.Id == query.TaskListId);
+            if (taskListDto == null)
+            {
+                return Error.NotFound("TaskList.NotFound", "The task list with the provided Id was not found.");
+            }
+
+            return taskListDto.TaskItems;
+        }
+    }
+}
