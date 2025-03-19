83bf7fca2259a4cc9d5906702d3c978fadee2683
Kevin Torrecilla <andrestorrecilla.14@gmail.com>
2025-03-18 19:52:34 -0500

    feat: return detailed DTO response instead of generic Unit

--- Application/TaskLists/Commands/UpdateTaskList/UpdateTaskListCommand.cs
+++ Application/TaskLists/Commands/UpdateTaskList/UpdateTaskListCommand.cs
@@ -1,7 +1,8 @@
-﻿using ErrorOr;
+﻿using Application.TaskLists.Dtos;
+using ErrorOr;
 using MediatR;
 
 namespace Application.TaskLists.Commands.UpdateTaskList
 {
-    public record UpdateTaskListCommand(Guid UserId, Guid TaskListId, string Name) : IRequest<ErrorOr<Unit>>;
+    public record UpdateTaskListCommand(Guid UserId, Guid TaskListId, string Name) : IRequest<ErrorOr<TaskListDTO>>;
 }
