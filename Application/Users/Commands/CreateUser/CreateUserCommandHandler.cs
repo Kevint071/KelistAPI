bd5dfcc0ebf57d46b06be14604d859197e86753f
Kevin Torrecilla <andrestorrecilla.14@gmail.com>
2025-03-18 20:06:50 -0500

    feat: return DTO from service instead of Unit in command handlers

--- Application/Users/Commands/CreateUser/CreateUserCommandHandler.cs
+++ Application/Users/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -5,10 +5,11 @@ using ErrorOr;
 using MediatR;
 using Application.Common;
 using Application.Users.Services;
+using Application.Users.Dtos;
 
 namespace Application.Users.Commands.CreateUser
 {
-    internal sealed class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, ErrorOr<Unit>>
+    internal sealed class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, ErrorOr<UserDTO>>
     {
         private readonly IUserService _userService;
 
@@ -17,7 +18,7 @@ namespace Application.Users.Commands.CreateUser
             _userService = userService ?? throw new ArgumentNullException(nameof(userService));
         }
 
-        public async Task<ErrorOr<Unit>> Handle(CreateUserCommand command, CancellationToken cancellationToken)
+        public async Task<ErrorOr<UserDTO>> Handle(CreateUserCommand command, CancellationToken cancellationToken)
         {
             var validationResult = ValueObjectValidator.ValidateUserValueObjects(command.Email, command.Name, command.LastName);
             if (validationResult.IsError) return validationResult.Errors;
@@ -25,8 +26,8 @@ namespace Application.Users.Commands.CreateUser
             var (name, lastname, email) = validationResult.Value;
             var user = new User(new UserId(Guid.NewGuid()), name, lastname, email);
 
-            await _userService.AddAsync(user, cancellationToken);
-            return Unit.Value;
+            var userDto = await _userService.AddAsync(user, cancellationToken);
+            return userDto;
         }
     }
 }
