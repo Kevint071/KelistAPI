85002231e2c28c36e0f901703256b3610369ddcf
Kevin Torrecilla <andrestorrecilla.14@gmail.com>
2025-03-20 22:29:29 -0500

    feat(auth): add AuthController with registration, login, and token refresh
    - Implemented `AuthController` to handle user authentication.
    - Added endpoints for user registration, login, and token refresh.
    - Included a protected test endpoint requiring authorization.

---
+++ Kelist.API/Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+﻿using Application.Users.Dtos;
+using Application.Users.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kelist.API.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController(IAuthService authService) : ApiController
+    {
+        private readonly IAuthService _authService = authService ?? throw new ArgumentNullException(nameof(authService));
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterUserDto request)
+        {
+            var result = await _authService.RegisterAsync(request);
+            return result.Match<IActionResult>(
+                user => Created($"/users/{user.Id}", user),
+                errors => Problem(errors)
+            );
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginUserDto request)
+        {
+            var result = await _authService.LoginAsync(request);
+            return result.Match<IActionResult>(
+                token => Ok(token),
+                errors => Problem(errors)
+            );
+        }
+
+        [HttpPost("refresh-token")]
+        public async Task<IActionResult> RefreshToken(RefreshTokenRequestDto request)
+        {
+            var result = await _authService.RefreshTokensAsync(request);
+            return result.Match<IActionResult>(
+                token => Ok(token),
+                errors => Problem(errors)
+            );
+        }
+
+        [Authorize]
+        [HttpGet("protected")]
+        public IActionResult ProtectedEndpoint()
+        {
+            return Ok("This is a protected endpoint.");
+        }
+    }
+}
