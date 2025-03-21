811ce80f1a10a0213694f52312d115730956ba2d
Kevin Torrecilla <andrestorrecilla.14@gmail.com>
2025-03-21 13:09:13 -0500

    feat(auth): integrate Vault for managing JWT key

--- Kelist.API/Program.cs
+++ Kelist.API/Program.cs
@@ -6,15 +6,48 @@ using Kelist.API.Middlewares;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
+using VaultSharp.V1.AuthMethods.Token;
+using VaultSharp;
+using DotNetEnv;
 
 namespace Kelist.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
+            Env.Load();
+
             var builder = WebApplication.CreateBuilder(args);
 
+            var vaultToken = Environment.GetEnvironmentVariable("VAULT_TOKEN");
+            if (string.IsNullOrEmpty(vaultToken)) throw new Exception("El token de Vault no está configurado en las variables de entorno.");
+
+            var authMethod = new TokenAuthMethodInfo(vaultToken);
+            var vaultClientSettings = new VaultClientSettings("http://localhost:8200", authMethod);
+            var vaultClient = new VaultClient(vaultClientSettings);
+
+            string jwtKey;
+            try
+            {
+                var secret = await vaultClient.V1.Secrets.KeyValue.V2.ReadSecretAsync(path: "jwt-signing-key", mountPoint: "secret");
+                jwtKey = secret.Data.Data["key"]?.ToString() ?? throw new InvalidOperationException("La clave JWT no se encontró en el secreto de Vault.");
+            }
+            catch (VaultSharp.Core.VaultApiException ex)
+            {
+                Console.WriteLine("Error al comunicarse con Vault");
+                Console.WriteLine($"Status Code: {ex.StatusCode}");
+                Console.WriteLine($"Message: {ex.Message}");
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error inesperado al leer el secreto de Vault");
+                Console.WriteLine($"Message: {ex.Message}");
+                return;
+            }
+
+            builder.Configuration["Jwt:Key"] = jwtKey;
             builder.Services.AddPresentation()
                             .AddInfrastructure(builder.Configuration)
                             .AddApplication();
@@ -29,7 +62,7 @@ namespace Kelist.API
                         ValidateAudience = true,
                         ValidAudience = builder.Configuration["Jwt:Audience"],
                         ValidateLifetime = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)),
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
                         ValidateIssuerSigningKey = true
                     };
                     options.Events = new JwtBearerEvents
