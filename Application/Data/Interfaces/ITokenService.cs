b0e59d11f567a1d9dafb03fcf259df68e3809835
Kevin Torrecilla <andrestorrecilla.14@gmail.com>
2025-03-20 22:22:00 -0500

    feat(auth): add token service for JWT and refresh tokens
    - Implemented `TokenService` to handle JWT creation and refresh token generation.
    - Registered `ITokenService` in dependency injection.

---
+++ Application/Data/Interfaces/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace Application.Data.Interfaces
+{
+    public interface ITokenService
+    {
+        string CreateJwtToken(IEnumerable<Claim> claims); // Cambiado para aceptar claims en lugar de User
+        string GenerateRefreshToken();
+    }
+}
