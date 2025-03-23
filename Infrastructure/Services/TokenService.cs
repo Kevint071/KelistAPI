5af328f441860cb587270a078c983d7cce071caf
Kevin Torrecilla <andrestorrecilla.14@gmail.com>
2025-03-22 21:38:55 -0500

    chore(token): clean up unused usings and improve code formatting

--- Infrastructure/Services/TokenService.cs
+++ Infrastructure/Services/TokenService.cs
@@ -3,7 +3,6 @@ using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 using Application.Data.Interfaces;
-using Domain.Users;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
