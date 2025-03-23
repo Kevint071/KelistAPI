cfbb5215a0d424871c4748fe9f049b9ea5802884
Kevin Torrecilla <andrestorrecilla.14@gmail.com>
2025-03-22 23:13:21 -0500

    feat(auth): add validators for user login and registration

---
+++ Application/AuthUsers/Command/RegisterUser/RegisterUserCommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using Application.Common.Validations;
+using FluentValidation;
+
+namespace Application.AuthUsers.Command.RegisterUser
+{
+    public partial class RegisterUserCommandValidator : AbstractValidator<RegisterUserCommand>
+    {
+        public RegisterUserCommandValidator()
+        {
+            RuleFor(r => r.Name)
+                .NotEmpty()
+                .MaximumLength(50);
+
+            RuleFor(r => r.LastName)
+                .NotEmpty()
+                .MaximumLength(50)
+                .WithName("Last Name");
+
+            RuleFor(r => r.Email)
+                .NotEmpty().WithMessage("El email es obligatorio.")
+                .EmailAddress().WithMessage("Se requiere un email válido.")
+                .MaximumLength(255).WithMessage("El email no debe exceder los 255 caracteres.");
+
+            RuleFor(r => r.Password)
+                .ApplyPasswordRules();
+        }
+    }
+}
