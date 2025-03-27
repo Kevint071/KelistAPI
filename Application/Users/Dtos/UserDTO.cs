15ae2ce2c1431054760f73e6a91a84c6001c1533
Kevin Torrecilla <andrestorrecilla.14@gmail.com>
2025-03-27 16:28:46 -0500

    refactor(user): convert UserDTO to primary constructor and add role

--- Application/Users/Dtos/UserDTO.cs
+++ Application/Users/Dtos/UserDTO.cs
@@ -1,33 +1,23 @@
-﻿using Application.TaskLists.Dtos;
+﻿using System.Data;
+using Application.TaskLists.Dtos;
 
 namespace Application.Users.Dtos
 {
-    public class UserDTO
+    public class UserDTO(Guid id, string personName, string lastName, string email, string passwordHash, string role = "User", string? refreshToken = null, DateTime? refreshTokenExpiryTime = null, List<TaskListDTO>? taskLists = null)
     {
-        public Guid Id { get; private set; }
-        public string PersonName { get; private set; }
-        public string LastName { get; private set; }
+        public Guid Id { get; private set; } = id;
+        public string PersonName { get; private set; } = personName;
+        public string LastName { get; private set; } = lastName;
         public string FullName => $"{PersonName} {LastName}";
-        public string Email { get; private set; }
-        public string PasswordHash { get; private set; }
-        public string? RefreshToken { get; private set; }
-        public DateTime? RefreshTokenExpiryTime { get; private set; }
-        public List<TaskListDTO> TaskLists { get; init; } = [];
-
-        public UserDTO(Guid id, string personName, string lastName, string email, string passwordHash, string? refreshToken = null, DateTime? refreshTokenExpiryTime = null, List<TaskListDTO>? taskLists = null)
-        {
-            Id = id;
-            PersonName = personName;
-            LastName = lastName;
-            Email = email;
-            PasswordHash = passwordHash;
-            RefreshToken = refreshToken;
-            RefreshTokenExpiryTime = refreshTokenExpiryTime;
-            TaskLists = taskLists ?? [];
-        }
+        public string Email { get; private set; } = email;
+        public string PasswordHash { get; private set; } = passwordHash;
+        public string Role { get; private set; } = role;
+        public string? RefreshToken { get; private set; } = refreshToken;
+        public DateTime? RefreshTokenExpiryTime { get; private set; } = refreshTokenExpiryTime;
+        public List<TaskListDTO> TaskLists { get; init; } = taskLists ?? [];
 
         // Constructor privado para EF Core
-        private UserDTO() : this(Guid.Empty, string.Empty, string.Empty, string.Empty, string.Empty) { }
+        private UserDTO() : this(Guid.Empty, string.Empty, string.Empty, string.Empty, string.Empty, "User") { }
 
         public void UpdateProfile(string name, string lastName, string email)
         {
